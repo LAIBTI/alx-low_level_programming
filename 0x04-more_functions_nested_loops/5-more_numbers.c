9204a2239c97c35363de30c340dbf8f9cd09264d
LAIBTI <ibtissam.lakhouil@edu.uca.ma>
2023-07-31 15:45:59 -0700

    5more

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,23 +1,21 @@
 #include "main.h"
 
 /**
- * print_most_numbers - checks for checks for a digit (0 through 9).
- *
- * Return: Always 0.
+ * more_numbers - prints 10 times the numbers, from 0 to 14
+ * followed by a new line
  */
-void print_most_numbers(void)
+void more_numbers(void)
 {
- int c;
+	int i, j;
 
- for (c = 48; c < 58; c++)
- {
- if (c != 50)
- {
- if (c != 52)
- {
- _putchar(c);
- }
- }
- }
- _putchar('\n');
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 15; j++)
+		{
+			if (j >= 10)
+				_putchar(j / 10 + '0');
+			_putchar(j % 10 + '0');
+		}
+		_putchar('\n');
+	}
 }
